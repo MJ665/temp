c560a49ba5154f1725e8803784ce9eaaa256d800
Meet Jain <meetj665@gmail.com>
2024-06-13 10:58:29 +0530

    traininign

--- trainingReactIP/src/App.tsx
+++ trainingReactIP/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -16,3 +13,5 @@ function App() {
 }
 
 export default App
+
+
